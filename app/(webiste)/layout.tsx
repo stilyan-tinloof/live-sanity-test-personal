e327d203adf08796f8d09a317e8e8b8918215c4f
stilyan.tinloof <stilyan.toskov@tinloof.com>
2024-11-18 08:11:04 +0000

    some more pages

--- app/(webiste)/layout.tsx
+++ app/(webiste)/layout.tsx
@@ -1,6 +1,7 @@
 import { SanityLive } from "@/sanity/lib/live";
 import { VisualEditing } from "next-sanity";
 import { draftMode } from "next/headers";
+import Link from "next/link";
 
 export default async function Layout({
   children,
@@ -9,6 +10,24 @@ export default async function Layout({
 }) {
   return (
     <div className="flex flex-col">
+      <header>
+        <nav>
+          <ul className="flex gap-3">
+            <li>
+              <Link href={"/"}>Home</Link>
+            </li>
+            <li>
+              <Link href={"/about"}>About</Link>
+            </li>
+            <li>
+              <Link href={"/blog"}>Blog</Link>
+            </li>
+            <li>
+              <Link href={"/contact"}>Contact</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
       <main className="mx-auto min-h-svh w-full flex-1">
         {children}
         <SanityLive />
--- app/(webiste)/page.tsx
+++ app/(webiste)/page.tsx
@@ -1,9 +1,3 @@
-import { Posts } from "@/components/Posts";
-import { sanityFetch } from "@/sanity/lib/live";
-import { BLOG_INDEX_QUERY } from "@/sanity/lib/queries";
-
-export default async function Home() {
-  const { data } = await sanityFetch({ query: BLOG_INDEX_QUERY });
-
-  return <Posts data={data} />;
+export default function Home() {
+  return <h1>Home page</h1>;
 }
